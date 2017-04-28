541e528cb6967039f5893400467f73eabb9f0355
PabloPiaggi <ppiaggi@gmail.com>
2017-04-28 17:07:00 +0200

    Regtest added

--- src/colvar/PairEntropy.cpp
+++ src/colvar/PairEntropy.cpp
@@ -32,6 +32,36 @@ using namespace std;
 namespace PLMD{
 namespace colvar{
 
+//+PLUMEDOC COLVAR PAIRENTROPY
+/*
+Calculate the global pair entropy using the expression:
+\f[
+s=-2\pi\rho k_B \int\limits_0^{r_{\mathrm{max}}} \left [ g(r) \ln g(r) - g(r) + 1 \right ] r^2 dr .
+\f]
+where \f$ g(r) $\f is the pair distribution function and \f$ r_{\mathrm{max}} $\f is a cutoff in the integration (MAXR).
+For the integration the interval from 0 to  \f$ r_{\mathrm{max}} $\f is partitioned in NHIST equal intervals. 
+To make the calculation of \f$ g(r) $\f differentiable, the following function is used:
+\f[
+g(r) = \frac{1}{4 \pi \rho r^2} \sum\limits_{j} \frac{1}{\sqrt{2 \pi \sigma^2}} e^{-(r-r_{ij})^2/(2\sigma^2)} ,
+\f]
+where \f$ \rho $\f is the density and \f$ sigma $\f is a broadening parameter (SIGMA).  
+\par Example)
+The following input tells plumed to calculate the pair entropy of atoms 1-250 with themselves.
+\verbatim
+PAIRENTROPY ...
+ LABEL=s2
+ GROUPA=1-250
+ MAXR=0.65
+ SIGMA=0.025
+ NHIST=100
+ NLIST
+ NL_CUTOFF=0.75
+ NL_STRIDE=10
+... PAIRENTROPY
+\endverbatim
+*/
+//+ENDPLUMEDOC
+
 class PairEntropy : public Colvar {
   bool pbc;
   bool serial;
@@ -44,13 +74,12 @@ class PairEntropy : public Colvar {
   double invSqrt2piSigma, sigmaSqr2, sigmaSqr;
   double deltar;
   unsigned deltaBin;
-  // Integration routine
+  // Integration routines
   double integrate(vector<double> integrand, double delta)const;
   Vector integrate(vector<Vector> integrand, double delta)const;
   Tensor integrate(vector<Tensor> integrand, double delta)const;
   // Kernel to calculate g(r)
   double kernel(double distance, double&der)const;
-  double CombinePerAtomValues(vector<double> entropies,Matrix<Vector> derivativesPerAtom, vector<Tensor> virialPerAtom, vector<Vector> &deriv, Tensor &virial)const;
 public:
   explicit PairEntropy(const ActionOptions&);
   ~PairEntropy();
@@ -109,7 +138,7 @@ firsttime(true)
    parse("NL_STRIDE",nl_st);
    if(nl_st<=0) error("NL_STRIDE should be explicitly specified and positive");
   }
-  
+
   addValueWithDerivatives(); setNotPeriodic();
   if(gb_lista.size()>0){
     if(doneigh)  nl= new NeighborList(ga_lista,gb_lista,dopair,pbc,getPbc(),nl_cut,nl_st);
@@ -118,9 +147,9 @@ firsttime(true)
     if(doneigh)  nl= new NeighborList(ga_lista,pbc,getPbc(),nl_cut,nl_st);
     else         nl= new NeighborList(ga_lista,pbc,getPbc());
   }
-  
+
   requestAtoms(nl->getFullAtomList());
- 
+
   log.printf("  between two groups of %u and %u atoms\n",static_cast<unsigned>(ga_lista.size()),static_cast<unsigned>(gb_lista.size()));
   log.printf("  first group:\n");
   for(unsigned int i=0;i<ga_lista.size();++i){
@@ -149,8 +178,10 @@ firsttime(true)
   parse("SIGMA",sigma);
   log.printf("The pair distribution function is calculated with a Gaussian kernel with deviation %f nm. \n", sigma);
   double rcut = maxr + 3*sigma;
-  rcut2 = (maxr + 3*sigma)*(maxr + 3*sigma);
-  if(nl_cut<rcut) error("NL_CUTOFF should be larger than MAXR + 3*SIGMA");
+  rcut2 = (maxr + 3*sigma)*(maxr + 3*sigma);  // 3*sigma is hard coded
+  if(doneigh){
+    if(nl_cut<rcut) error("NL_CUTOFF should be larger than MAXR + 3*SIGMA");
+  }
 
   checkRead();
 
@@ -160,7 +191,7 @@ firsttime(true)
   sigmaSqr2 = 2.*sigma*sigma;
   sigmaSqr = sigma*sigma;
   deltar=maxr/nhist;
-  deltaBin = std::floor(3*sigma/deltar);
+  deltaBin = std::floor(3*sigma/deltar); // 3*sigma is hard coded
 }
 
 PairEntropy::~PairEntropy(){
@@ -185,9 +216,15 @@ void PairEntropy::prepare(){
 // calculator
 void PairEntropy::calculate()
 {
-  vector<double> entropies;
-  Matrix<Vector> derivativesPerAtom(getNumberOfAtoms(),getNumberOfAtoms());
-  vector<Tensor> virialPerAtom(getNumberOfAtoms());
+  // Define output quantities
+  double pairEntropy;
+  vector<Vector> deriv(getNumberOfAtoms());
+  Tensor virial;
+  // Define intermediate quantities
+  vector<double> gofr(nhist);
+  vector<double> logGofr(nhist);
+  Matrix<Vector> gofrPrime(nhist,getNumberOfAtoms());
+  vector<Tensor> gofrVirial(nhist);
   // Setup neighbor list and parallelization
   if(nl->getStride()>0 && invalidateList){
     nl->update(getPositions());
@@ -201,113 +238,110 @@ void PairEntropy::calculate()
     stride=comm.Get_size();
     rank=comm.Get_rank();
   }
-  // Calculate volume and density
-  double volume=getBox().determinant(); 
-  double density=getNumberOfAtoms()/volume;
-  // Loop over central atoms
-  for(unsigned int i=rank;i<getNumberOfAtoms();i+=stride) {   
-    vector<double> gofr(nhist);
-    vector<double> logGofr(nhist);
-    Matrix<Vector> gofrPrime(nhist,getNumberOfAtoms());
-    vector<Tensor> gofrVirial(nhist);
-    //vector<Vector> deriv(getNumberOfAtoms());
-    //Tensor virial;
+  // Loop over neighbors
+  const unsigned nn=nl->size();
+  for(unsigned int i=rank;i<nn;i+=stride) {
     double dfunc, d2;
     Vector distance;
     Vector distance_versor;
-    std::vector<unsigned> neighbors;
-    neighbors=nl->getNeighbors(i);
-    // Loop over neighbors
-    for(unsigned int j=0;j<neighbors.size();j+=1) { 
-      distance = pbcDistance(getPosition(i),getPosition(neighbors[j]));
-      if ( (d2=distance[0]*distance[0])<rcut2 && (d2+=distance[1]*distance[1])<rcut2 && (d2+=distance[2]*distance[2])<rcut2) {
-        double distanceModulo=std::sqrt(d2);
-        Vector distance_versor = distance / distanceModulo;
-        unsigned bin=std::floor(distanceModulo/deltar);
-        int minBin, maxBin; // These cannot be unsigned
-        // Only consider contributions to g(r) of atoms less than n*sigma bins apart from the actual distance
-        minBin=bin - deltaBin;
-        if (minBin < 0) minBin=0;
-        if (minBin > (nhist-1)) minBin=nhist-1;
-        maxBin=bin +  deltaBin;
-        if (maxBin > (nhist-1)) maxBin=nhist-1;
-        for(int k=minBin;k<maxBin+1;k+=1) {   
-          double x=deltar*(k+0.5);
-          gofr[k] += kernel(x-distanceModulo, dfunc);
-          Vector value = dfunc * distance_versor;
-          gofrPrime[k][i] += value;
-          gofrPrime[k][j] -= value;
-          Tensor vv(value, distance);
-          gofrVirial[k] += vv;
-        } 
-      }
+    unsigned i0=nl->getClosePair(i).first;
+    unsigned i1=nl->getClosePair(i).second;
+    if(getAbsoluteIndex(i0)==getAbsoluteIndex(i1)) continue;
+    if(pbc){
+     distance=pbcDistance(getPosition(i0),getPosition(i1));
+    } else {
+     distance=delta(getPosition(i0),getPosition(i1));
     }
-    // Normalize g(r)
-    for(unsigned j=0;j<nhist;++j){
-      double x=deltar*(j+0.5);
-      double normConstant = 4*pi*density*x*x;
-      gofr[j] /= normConstant;
-      gofrVirial[j] /= normConstant;
-      for(unsigned k=0;k<getNumberOfAtoms();++k){
-        gofrPrime[j][k] /= normConstant;
-      }
-    }
-    // Construct integrand
-    vector<double> integrand(nhist);
-    for(unsigned j=0;j<nhist;++j){
-      double x=deltar*(j+0.5);
-      logGofr[j] = std::log(gofr[j]);
-      if (gofr[j]<1.e-10) {
-        integrand[j] = x*x;
-      } else {
-        integrand[j] = (gofr[j]*logGofr[j]-gofr[j]+1)*x*x;
-      }
-    }
-    // Integrate to obtain pair entropy;
-    entropies[i] = -2*pi*density*integrate(integrand,deltar); 
-    // Construct integrand and integrate derivatives
-    for(unsigned j=0;j<getNumberOfAtoms();++j) {
-      vector<Vector> integrandDerivatives(nhist);
-      for(unsigned k=0;k<nhist;++k){
+    if ( (d2=distance[0]*distance[0])<rcut2 && (d2+=distance[1]*distance[1])<rcut2 && (d2+=distance[2]*distance[2])<rcut2) {
+      double distanceModulo=std::sqrt(d2);
+      Vector distance_versor = distance / distanceModulo;
+      unsigned bin=std::floor(distanceModulo/deltar);
+      int minBin, maxBin; // These cannot be unsigned
+      // Only consider contributions to g(r) of atoms less than n*sigma bins apart from the actual distance
+      minBin=bin - deltaBin;
+      if (minBin < 0) minBin=0;
+      if (minBin > (nhist-1)) minBin=nhist-1;
+      maxBin=bin +  deltaBin;
+      if (maxBin > (nhist-1)) maxBin=nhist-1;
+      for(int k=minBin;k<maxBin+1;k+=1) {
         double x=deltar*(k+0.5);
-        if (gofr[k]>1.e-10) {
-          integrandDerivatives[k] = gofrPrime[k][j]*logGofr[k]*x*x;
-        }
-      }
-      // Integrate
-      derivativesPerAtom[i][j] = -2*pi*density*integrate(integrandDerivatives,deltar);
-    }
-    // Virial of positions
-    // Construct virial integrand
-    vector<Tensor> integrandVirial(nhist);
-    for(unsigned j=0;j<nhist;++j){
-      double x=deltar*(j+0.5);
-      if (gofr[j]>1.e-10) {
-        integrandVirial[j] = gofrVirial[j]*logGofr[j]*x*x;
+        gofr[k] += kernel(x-distanceModulo, dfunc);
+        Vector value = dfunc * distance_versor;
+        gofrPrime[k][i0] += value;
+        gofrPrime[k][i1] -= value;
+        Tensor vv(value, distance);
+        gofrVirial[k] += vv;
       }
     }
-    // Integrate virial
-    virialPerAtom[i] = -2*pi*density*integrate(integrandVirial,deltar);
-    // Virial of volume
-    // Construct virial integrand
-    vector<double> integrandVirialVolume(nhist);
-    for(unsigned j=0;j<nhist;j+=1) {   
-      double x=deltar*(j+0.5);
-      integrandVirialVolume[j] = (-gofr[j]+1)*x*x;
-    }
-    // Integrate virial
-    virialPerAtom[i] += -2*pi*density*integrate(integrandVirialVolume,deltar)*Tensor::identity();
-  } 
-  if(!serial){
-    comm.Sum(&entropies[0],getNumberOfAtoms());
-    comm.Sum(&derivativesPerAtom[0][0][0],getNumberOfAtoms()*getNumberOfAtoms()*3);
-    comm.Sum(&virialPerAtom[0][0][0],getNumberOfAtoms()*3*3);
   }
-  vector<Vector> deriv(getNumberOfAtoms());
-  Tensor virial;
-  double CVvalue = CombinePerAtomValues(entropies, derivativesPerAtom, virialPerAtom, deriv, virial);
+  if(!serial){
+    comm.Sum(&gofr[0],nhist);
+    comm.Sum(&gofrPrime[0][0],nhist*getNumberOfAtoms());
+    comm.Sum(&gofrVirial[0],nhist);
+  }
+  // Calculate volume and density
+  double volume=getBox().determinant();
+  double density=getNumberOfAtoms()/volume;
+  // Normalize g(r)
+  double normConstantBase = 2*pi*getNumberOfAtoms()*density;
+  for(unsigned j=0;j<nhist;++j){
+    double x=deltar*(j+0.5);
+    double normConstant = normConstantBase*x*x;
+    gofr[j] /= normConstant;
+    //log.printf(" gofr after %f %f \n",x, gofr[j]);
+    gofrVirial[j] /= normConstant;
+    for(unsigned k=0;k<getNumberOfAtoms();++k){
+      gofrPrime[j][k] /= normConstant;
+    }
+  }
+  // Construct integrand
+  vector<double> integrand(nhist);
+  for(unsigned j=0;j<nhist;++j){
+    double x=deltar*(j+0.5);
+    logGofr[j] = std::log(gofr[j]);
+    if (gofr[j]<1.e-10) {
+      integrand[j] = x*x;
+    } else {
+      integrand[j] = (gofr[j]*logGofr[j]-gofr[j]+1)*x*x;
+    }
+  }
+  // Integrate to obtain pair entropy;
+  pairEntropy = -2*pi*density*integrate(integrand,deltar);
+  // Construct integrand and integrate derivatives
+  for(unsigned j=0;j<getNumberOfAtoms();++j) {
+    vector<Vector> integrandDerivatives(nhist);
+    for(unsigned k=0;k<nhist;++k){
+      double x=deltar*(k+0.5);
+      if (gofr[k]>1.e-10) {
+        integrandDerivatives[k] = gofrPrime[k][j]*logGofr[k]*x*x;
+      }
+    }
+    // Integrate
+    deriv[j] = -2*pi*density*integrate(integrandDerivatives,deltar);
+  }
+  // Virial of positions
+  // Construct virial integrand
+  vector<Tensor> integrandVirial(nhist);
+  for(unsigned j=0;j<nhist;++j){
+    double x=deltar*(j+0.5);
+    if (gofr[j]>1.e-10) {
+      integrandVirial[j] = gofrVirial[j]*logGofr[j]*x*x;
+    }
+  }
+  // Integrate virial
+  virial = -2*pi*density*integrate(integrandVirial,deltar);
+  // Virial of volume
+  // Construct virial integrand
+  vector<double> integrandVirialVolume(nhist);
+  for(unsigned j=0;j<nhist;j+=1) {
+    double x=deltar*(j+0.5);
+    integrandVirialVolume[j] = (-gofr[j]+1)*x*x;
+  }
+  // Integrate virial
+  virial += -2*pi*density*integrate(integrandVirialVolume,deltar)*Tensor::identity();
+  // Assign output quantities
   for(unsigned i=0;i<deriv.size();++i) setAtomsDerivatives(i,deriv[i]);
-  setValue           (CVvalue);
+  setValue           (pairEntropy);
   setBoxDerivatives  (virial);
 }
 
@@ -353,21 +387,6 @@ Tensor PairEntropy::integrate(vector<Tensor> integrand, double delta)const{
   result *= delta;
   return result;
 }
-
-double CombinePerAtomValues(vector<double> entropies,Matrix<Vector> derivativesPerAtom, vector<Tensor> virialPerAtom, vector<Vector> &deriv, Tensor &virial) {
-  // Sum function
-  double result;
-  for (unsigned int i=0;i<entropies.size();++i) result += entropies[i];
-  for (unsigned int i=0;i<entropies.size();++i) {
-    for (unsigned int j=0;j<entropies.size();++j) {
-      deriv[i] += derivativesPerAtom[j][i];
-    }
-  }
-  for (unsigned int i=0; i<entropies.size();++i) virial += virialPerAtom[i];
-  return result;
-}
-
-
 
 }
 }
